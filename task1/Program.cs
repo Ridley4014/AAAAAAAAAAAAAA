8a5f8482da7b533eded94fc81443288823c2a46d
Ridley <vasandrew3@gmail.com>
2024-04-08 09:08:09 +1000

    Filthy piece of code

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.WriteLine("Fuck off!");
+
+int value = 100;
+Console.WriteLine(value);
+
+
+double d = 4.38;
+char c = 'I';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
